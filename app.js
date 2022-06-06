3d328786c594efeed3bacc294decbfd0dd5d58e4
Philip Choi <ceeeoh@gmail.com>
2022-06-06 12:38:58 +0800

    git init

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+// Module
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+// Routing
+const home = require('./routes/home');
+
+
+
+
+// App Setting
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.use('/', home);
+
+module.exports = app;
