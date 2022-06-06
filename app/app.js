4486a3eb0706fd080a014c011c4baca7cd12ce72
Philip Choi <ceeeoh@gmail.com>
2022-06-06 13:29:30 +0800

    directory optimised

---
+++ app/app.js
@@ -0,0 +1,20 @@
+"use strict";
+// Module
+const express = require('express');
+const app = express();
+
+
+
+// Routing
+const home = require('./src/routes/home');
+
+
+
+
+// App Setting
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+app.use('/', home);
+
+module.exports = app;
