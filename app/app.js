29bfdc1553f117c1d43cbf22a8d40c36da307dc9
Philip Choi <ceeeoh@gmail.com>
2022-06-06 13:56:02 +0800

    front js file added

--- app/app.js
+++ app/app.js
@@ -8,13 +8,11 @@ const app = express();
 // Routing
 const home = require('./src/routes/home');
 
-
-
-
 // App Setting
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/', home);
+app.use(express.static(`${__dirname}/src/public`));
 
 module.exports = app;
